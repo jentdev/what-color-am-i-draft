7c0af52fd0d8892c5190a4afd5acab13fff7d4a3
Jennie <jentdev@gmail.com>
2023-09-05 15:30:44 -0700

    added color squares

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,12 @@
+// -- target random color div
+let randomColor = document.getElementById('random-color');
 
+// -- generate a random number and * 255
+let rRed = Math.floor(Math.random() * 256);
+let rGreen = Math.floor(Math.random() * 256);
+let rBlue = Math.floor(Math.random() * 256);
+let rRGB = `rgb(${rRed}, ${rGreen}, ${rBlue})`;
+randomColor.style.background = rRGB;
 
 // call changeColor when slider is used
 document.getElementById('range-red').addEventListener('input', changeColor);
@@ -25,7 +33,8 @@ function changeColor () {
     printGreen.innerHTML = green;
     printBlue.innerHTML = blue;
     
-    // change body bg to color
-    document.body.style.background = color;
+    // update guessed color
+    let guessedColor = document.getElementById('guessed-color');
+    guessedColor.style.background = color;
 }
 
