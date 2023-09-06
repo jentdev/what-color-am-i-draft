87ca8ed068fd991e58bd1da14b4d19d18810a490
Jennie <jentdev@gmail.com>
2023-09-06 16:23:38 -0700

    finalize mini game

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,9 @@ newGame.addEventListener('click', clearResult);
 
 // function to hide result when play again
 function clearResult () {
+    currentColor = '0, 0, 0';
+    let btnSubmit = document.getElementById('submit');
+    btnSubmit.disabled = false;
     let rangeRed = document.getElementById('range-red');
     let rangeGreen = document.getElementById('range-green');
     let rangeBlue = document.getElementById('range-blue');
@@ -35,12 +38,14 @@ function clearResult () {
     let displayGuessed = document.getElementById('guessed-result');
     let clearGuessedRGB = document.getElementById('guessed-rgb');
     let clearGuessedHex = document.getElementById('guessed-hex');
-    clearGuessedHex.innerHTML = '';
-    clearGuessedRGB.innerHTML = '';  
+    clearGuessedHex.innerHTML = '#000000';
+    clearGuessedRGB.innerHTML = '(0, 0, 0)';  
     randomColor = generateColor();
     displayColor(randomColor);
     displayContainer.style.display = 'none';
     displayGuessed.style.display = 'block';
+    let hideScores = document.getElementById('super-result');
+    hideScores.style.visibility = 'hidden';
 }
 
 // function used to call displayResult
@@ -60,8 +65,16 @@ function displayResult (color) {
     displayRandomRGB.innerHTML = `(${color})`;
     displayRandomHex.innerHTML = `${rgbToHex(color).toUpperCase()}`;
     displayContainer.style.display = 'block';
-
-    calc(randomColor, currentColor);
+    let hideScores = document.getElementById('super-result');
+    hideScores.style.visibility = 'visible';
+    let score = calc(randomColor, currentColor);
+    let printPhrase = document.getElementById('phrase');
+    let phraseResult = checkScore(Number(score));
+    printPhrase.innerHTML = phraseResult;
+    percentColor(Number(score));
+    let btnSubmit = document.getElementById('submit');
+    btnSubmit.disabled = true;
+    printPercent(score);
 }
 
 // function to generate random number
@@ -113,6 +126,83 @@ function calc (random, guessed) {
     let rDiff = Math.abs(rResult[0] - gResult[0]);
     let gDiff = Math.abs(rResult[1] - gResult[1]);
     let bDiff = Math.abs(rResult[2] - gResult[2]);
-    let percentage = ((255 - rDiff) / 2.55) + ((255 - gDiff) / 2.55) + ((255 - bDiff) / 2.55);
-    return (percentage / 3).toFixed(2);
+
+    let percentage = 100 - (((rDiff / 255) + (gDiff / 255) + (bDiff / 255)) / 3 * 100);
+    return (percentage.toFixed(2));
+}
+
+// RESULTS //
+
+// function to print percent to screen
+function printPercent (percent) {
+    let printPercentResult = document.getElementById('percent');
+    printPercentResult.innerHTML = `${percent}%`;
+}
+
+// < 90
+let losePhrases = [
+    `You didn't fail. You just found a creative way to get a really low score.`,
+    `You've set a new record for low scores. It's like you won the Olympic gold in failing.`,
+    `Why did you take a detour to the Land of Failure?`,
+    `I'm impressed. That was an epic fail.`,
+    `I didn't expect this from you...`,
+    `How did you come up with this answer?`,
+];
+// 90 - 94.99 
+let winPhrases = [
+    `You deserve a pat on the back!`,
+    `You're on fire!`,
+    `Umm... Was that a lucky guess? Good job though.`,
+    `Well done! I can't wait to see what you accomplish next.`,
+    `Way to go!`,
+];
+
+// 95 - 99.99
+let betterPhrases = [
+    `All hail the Champ!`,
+    `Way to go, superstar!`,
+    `Good job, genius!`,
+    `You deserve a standing ovation!`,
+    `Congratulations. Now get back to work!`,
+    `Take a bow, you deserve it!`,
+    `You definitely get a gold star for this one!`,
+    `Are you trying to make everyone jealous? If so, it's working!`,
+];
+// 100
+let perfectPhrases = [
+    `Okay... Did you cheat?`,
+];
+
+// function to choose a random phrase
+function pickAPhrase (arr) {
+    let length = arr.length;
+    let theChosenOne = Math.floor(Math.random() * length);
+    return arr[theChosenOne];
+}
+
+// pass in score, print fail/pass phrase
+function checkScore (score) {
+    if (score === 100) {
+        return pickAPhrase(perfectPhrases);
+    }
+    else if (score > 95) {
+        return pickAPhrase(betterPhrases);
+    }
+    else if (score > 90) {
+        return pickAPhrase(winPhrases);
+    }
+    return pickAPhrase(losePhrases);
+}
+
+// function to choose font color for percent
+function percentColor (score) {
+    let percentField = document.getElementById('percent');
+    let chosenColor = '';
+    if (score < 90) {
+        chosenColor = `--clr-failed`;
+    }
+    else {
+        chosenColor = `--clr-passed`;
+    }
+    percentField.style.color = `var(${chosenColor})`;
 }
