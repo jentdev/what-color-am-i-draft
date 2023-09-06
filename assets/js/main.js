b39b54cd76534d0e7b331e72933c1f3fbb1b4bba
Jennie <jentdev@gmail.com>
2023-09-05 22:00:01 -0700

    playing around with js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,48 @@
-// -- target random color div
-let randomColor = document.getElementById('random-color');
 
-// -- generate a random number and * 255
-let rRed = Math.floor(Math.random() * 256);
-let rGreen = Math.floor(Math.random() * 256);
-let rBlue = Math.floor(Math.random() * 256);
-let rRGB = `rgb(${rRed}, ${rGreen}, ${rBlue})`;
-randomColor.style.background = rRGB;
 
-// call changeColor when slider is used
-document.getElementById('range-red').addEventListener('input', changeColor);
-document.getElementById('range-green').addEventListener('input', changeColor);
-document.getElementById('range-blue').addEventListener('input', changeColor);
 
-// function to change color using vals from slider
-function changeColor () {
-    // select each slider
-    let red = document.getElementById('range-red').value;
-    let green = document.getElementById('range-green').value;
-    let blue = document.getElementById('range-blue').value;
 
-    // construct rgb() value for bg
-    let color = `rgb(${red}, ${green}, ${blue})`;
 
-    // select where to print each color
-    let printRed = document.getElementById('print-red');
-    let printGreen = document.getElementById('print-green');
-    let printBlue = document.getElementById('print-blue');
 
-    // print color value to dom
-    printRed.innerHTML = red;
-    printGreen.innerHTML = green;
-    printBlue.innerHTML = blue;
-    
-    // update guessed color
-    let guessedColor = document.getElementById('guessed-color');
-    guessedColor.style.background = color;
-}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', displayColor);
+
+// let button = document.getElementById('submit');
+// button.addEventListener('click', displayResult);
+
+// // function to display result after submit
+// function displayResult () {
+//     let displayRandomRGB = document.getElementById('random-rgb');
+//     let displayRandomHex = document.getElementById('random-hex');
+//     displayRandomRGB.innerHTML = `(${generateColor()})`;
+//     displayRandomHex = 'something';
+// }
+
+// // function to generate random number
+// function generateColor () {
+//     let randomRed = Math.floor(Math.random() * 256);
+//     let randomGreen = Math.floor(Math.random() * 256);
+//     let randomBlue = Math.floor(Math.random() * 256);
+//     let randomColor = `${randomRed}, ${randomGreen}, ${randomBlue}`; 
+//     return randomColor;
+// }
+
+// // function to display generated color to dom
+// function displayColor () {
+//     let displayRandomColor = document.getElementById('random-color');
+//     let getColor = generateColor();
+//     displayRandomColor.style.background = `rgb(${getColor})`;
+// }
